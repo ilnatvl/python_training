1688e42f4252feee3670e0dbd51261a52b15a7b1
ilnatvl <ilnatvl@gmail.com>
2015-03-17 22:53:40 +0100

    Создан новый тест для удаления группы

--- fixture/group.py
+++ fixture/group.py
@@ -27,7 +27,16 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
         self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
 
     def return_to_groups_page(self):
         wd = self.app.wd
