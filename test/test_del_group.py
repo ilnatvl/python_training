b7a16f4f60c806cf270b1b97e0a873b6083909c2
ilnatvl <ilnatvl@gmail.com>
2015-03-30 22:28:56 +0200

    Добавлено предусловие перед тестом на удаление группы

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = 'nata'
 
+from model.group import Group
+
 
 def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.delete_first_group()
