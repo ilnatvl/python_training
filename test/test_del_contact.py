d74225f1ab367137a3eabe55ede00b1eb7be70e9
ilnatvl <ilnatvl@gmail.com>
2015-04-01 22:28:38 +0200

    Добавлена проверка предусловия в тест на удаление контакта

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 __author__ = 'nata'
+from model.contact import Contact
 
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="first name"))
     app.contact.delete_first_contact()
+
+
