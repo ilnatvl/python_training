49b1dfcb9741b6381b326f516b8a006c23efc22a
ilnatvl <ilnatvl@gmail.com>
2015-04-01 22:39:32 +0200

    Добавлены предусловия для тестов на модификацию
    Добавлены предусловия на наличие хотя бы одного объекта в тесты на
    модификацию групп и контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,9 +2,13 @@
 from model.contact import Contact
 
 
-def test_modify_first_name(app):
+def test_modify_contact_first_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="first name"))
     app.contact.modify_first_contact(Contact(first_name="New first name"))
 
 
-def test_modify_middle_name(app):
+def test_modify_contact_middle_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="first name"))
     app.contact.modify_first_contact(Contact(middle_name="dlfjoir"))
