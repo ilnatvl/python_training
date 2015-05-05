7addf96925c639ae873069cd0ec7ab0fab8bf630
ilnatvl <ilnatvl@gmail.com>
2015-05-05 22:37:07 +0200

    Реализованы дополнительные опции командной строки
    Реализованы дополнительные опции командной строки для указания типа
    браузера и адреса приложения

--- conftest.py
+++ conftest.py
@@ -10,11 +10,14 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseurl")
     if fixture is None:
-        fixture = Application()
+
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -22,7 +25,16 @@ def app(request):
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        fixture.session.logout()
-        fixture.destroy()
+        try:
+            fixture.session.logout()
+            fixture.destroy()
+        except AttributeError:
+            pass
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseurl", action="store", default="http://localhost/addressbook/")
+
--- fixture/application.py
+++ fixture/application.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError ("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -22,8 +30,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not ("/index.php" in wd.current_url and len(wd.find_elements_by_name("add")) > 0):
-            # wd.get("http://dstation.local/addressbook/index.php")
-            wd.get("http://localhost/addressbook/index.php")
+            wd.get(self.base_url)
 
     def return_to_home_page(self):
         wd = self.wd
