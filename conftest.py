9dd9c1f38887f2e82bfa7cb9ced2e33ef7047363
ilnatvl <ilnatvl@gmail.com>
2015-05-07 02:54:03 +0200

    Реализована загрузка тестовой конфигурации из файла
    Реализована загрузка тестовой конфигурации из json файла.

--- conftest.py
+++ conftest.py
@@ -1,28 +1,26 @@
 # -*- coding: utf-8 -*-
 __author__ = 'nata'
 
-from fixture.application import Application
+import json
+import os.path
 import pytest
+from fixture.application import Application
 
 fixture = None
-
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseurl")
-    username = request.config.getoption("--username")
-    password = request.config.getoption("--password")
-    if not password:
-        raise ValueError("Please provide password with --password option")
-
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username=username, password=password)
+    if target is None:
+        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_path) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -40,7 +38,5 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseurl", action="store", default="http://localhost/addressbook/")
-    parser.addoption("--username", action="store", default="admin")
-    parser.addoption("--password", action="store")
+    parser.addoption("--target", action="store", default="target.json")
 
